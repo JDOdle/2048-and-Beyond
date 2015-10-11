9644c6aab414b46980001b84d7e4ee5a81ce7bac
JDOdle <jdodle@email.arizona.edu>
2015-10-11 12:01:25 -0700

    Selenium Version Change
    reverting from 2.47.1 to 2.45.0 due to some runtime issues

--- src/game/play2048.java
+++ src/game/play2048.java
@@ -4,16 +4,16 @@ import tools.AI2048;
 
 public class play2048 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		AI2048 ai = new AI2048();
 		
 		if (!ai.startGame()) return;
   		
-  		while (!ai.gameOver())
-  			ai.makeMove(3);
-  		
+  		while (!ai.gameOver()) {
+  			ai.makeMove(3); 
+  			Thread.sleep(500);
+  		}
   		ai.quitGame();
 	}
-
 }
